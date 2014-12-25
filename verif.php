49fcc87b355be9ede06236adfd906212ffb549bc
Magnarus <delau.greg@gmail.com>
2014-12-25 23:50:50 +0100

    upgrade securité de l'inscription contre failles diverses

--- verif.php
+++ verif.php
@@ -1,28 +1,32 @@
 <?php
 	include 'conBD.php';
 
-	if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['mdp']))
+	if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['mdp']) && isset($_POST['mdp2']))
 	{
-		$nom = $_POST['nom'];
-		$prenom = $_POST['prenom'];
-		$email = $_POST['email'];
-		$mdp = $_POST['mdp'];
+		sleep(1);
+		$nom = htmlspecialchars($_POST['nom']);
+		$prenom = htmlspecialchars($_POST['prenom']);
+		$email = htmlspecialchars($_POST['email']);
+		$mdp = htmlspecialchars($_POST['mdp']);
+		$mdp2 = htmlspecialchars($_POST['mdp2']);
 
 		if(filter_var($email,FILTER_VALIDATE_EMAIL))
 		{
 			$mdp = sha1(md5("sel1") + $mdp + md5("sel2"));
-			$vSql = "INSERT INTO UTILISATEUR (nom,prenom,email,mdp) values ( '$nom','$prenom','$email','$mdp')";
-			$vQuery = pg_query($vConn,$vSql);
-			if(!$vQuery)
+			$mdp2 = sha1(md5("sel1") + $mdp2 + md5("sel2"));
+			if(strcmp($mdp,$mdp2) == 0)
 			{
-				echo "erreur lors de la requête : \n";
-				echo $vSql. "\n";
-				exit;
+               $res = pg_prepare($vConn,"insert","INSERT INTO UTILISATEUR(nom,prenom,email,mdp) VALUES($1, $2, $3, $4)");
+               $res = pg_execute($vConn,"insert",array($nom,$prenom,$email,$mdp));
 			}
 			else
 			{
-				echo "insertion de l'utilisateur $nom reussie \n";
+				echo "les mots de passes saisis sont differents\n";
 			}
+		}
+		else
+		{
+			echo "l'adresse mail saisie n'est pas valide \n";
 		}
 	}
 	pg_close($vConn);
