dc42798b5597ed48057046d9b78a4351e8bee99f
Yanonix <yanonix@gmail.com>
2015-01-04 03:58:48 +0100

    Fix double <form> pour inscription + class CSS ajouté pour cas spécifique

--- connexion.php
+++ connexion.php
@@ -15,7 +15,7 @@ if(isset($needHTML))
 }
 ?>
 
-<form method="POST" action="connexion.php">
+<form method="POST" action="connexion.php" class="table">
 	<label for="email">Email : </label><input type="text" name="email" id="email" value="<?=defaultPost('email')?>"/><br/>
 	<label for="mdp">Mot de passe : </label><input type="password" name="mdp" id="mdp" value="<?=defaultPost('mdp')?>"/><br/>
 	<input type="submit"/>
